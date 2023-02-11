88655b8816a726a087fbfc57f5167ac93e6e41df
Jal Gami <117020965+Jal-Gami@users.noreply.github.com>
2023-02-11 10:05:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,41 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+      arr.map((student) => {
+        if (student.marks > 50) {
+          console.log(student);
+        }
+      });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+      arr.forEach(function(ele){
+        if(ele.marks>=50){
+            console.log(ele);
+        }
+    })
 }
 
 function addData() {
   //Write your code here, just console.log
+      let add = {id:4,name:"susan",age:"20",marks: 45};
+      arr.push(add);
+      console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+      for(let i=0;i<arr.length;i++){
+        if(arr[i].marks<50){
+            arr.pop(arr[i]);
+        }
+      }
+      console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+      let concatearray = arr.concat(arr2);
+      console.log(concatearray);
 }
